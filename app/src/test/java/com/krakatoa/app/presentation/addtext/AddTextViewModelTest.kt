7686066ac6bc00f9eda496da8d7d32fb1ff5fa21
Tiago Vidotto <tiagovidotto@gamil.com>
2025-06-04 15:05:38 -0300

    added texts

--- app/src/test/java/com/krakatoa/app/presentation/addtext/AddTextViewModelTest.kt
+++ app/src/test/java/com/krakatoa/app/presentation/addtext/AddTextViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.krakatoa.app.presentation.addtext
 
+import androidx.lifecycle.viewmodel.compose.viewModel
 import app.cash.turbine.test
 import com.krakatoa.app.domain.usecase.SendTextUseCase
 import kotlinx.coroutines.test.runTest
@@ -7,6 +8,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.mock
+import kotlin.test.assertEquals
 
 
 class AddTextViewModelTest {
@@ -28,6 +30,49 @@ class AddTextViewModelTest {
             assertTrue(idle is AddTextUiState.Idle)
             assertTrue(loading is AddTextUiState.Loading)
             assertTrue(success is AddTextUiState.Success)
+        }
+    }
+
+    @Test
+    fun `addText emits ShowSnackBar and NavigationBack events`() = runTest {
+        val fakeUseCase = mock<SendTextUseCase>() {
+            onBlocking { invoke("Hello") } doAnswer {}
+        }
+        val viewModel = AddTextViewModel(fakeUseCase)
+
+        viewModel.uiEvent.test {
+            viewModel.addText("Hello")
+
+            val snackBarEvent = awaitItem()
+            val navigationEvent = awaitItem()
+
+            assertTrue(snackBarEvent is AddTextUiEvent.ShowSnackBar)
+            assertEquals("Text added successfully!", (snackBarEvent as AddTextUiEvent.ShowSnackBar).message)
+
+            assertEquals(AddTextUiEvent.NavigationBack, navigationEvent)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `addTest emits ShowSnackBar on error`() = runTest {
+        val fakeUseCase = mock<SendTextUseCase>() {
+            onBlocking { invoke("Hello") } doAnswer {
+                throw RuntimeException("Network error")
+            }
+        }
+
+        val viewModel = AddTextViewModel(fakeUseCase)
+
+        viewModel.uiEvent.test {
+            viewModel.addText("Hello")
+
+            val errorEvent = awaitItem()
+            assertTrue(errorEvent is AddTextUiEvent.ShowSnackBar)
+            assertTrue((errorEvent as AddTextUiEvent.ShowSnackBar).message.contains("Network error"))
+
+            cancelAndIgnoreRemainingEvents()
         }
     }
 }
